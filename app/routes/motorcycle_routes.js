bee662fe24eb4afc942c86ae1c4005b3cd7b5fab
christian-the-brewer <brewer.christiang@gmail.com>
2022-08-09 14:05:49 -0400

    Updates routes to not use auth

--- app/routes/motorcycle_routes.js
+++ app/routes/motorcycle_routes.js
@@ -55,7 +55,7 @@ router.get('/motorcycles/:id', (req, res, next) => {
 
 //CREATE------------------------
 //POST /motorcycles
-router.post('/motorcycles', requireToken, (req, res, next) => {
+router.post('/motorcycles', (req, res, next) => {
     //set owner
     req.body.motorcycle.owner = req.user.id
 
@@ -70,17 +70,11 @@ router.post('/motorcycles', requireToken, (req, res, next) => {
 
 //UPDATE-------------------
 //PATCH /motorcycles/:id
-router.patch('/motorcycles/:id', requireToken, removeBlanks, (req, res, next) => {
-    // if the client attempts to change the `owner` property by including a new
-    // owner, prevent that by deleting that key/value pair
-    delete req.body.motorcycle.owner
+router.patch('/motorcycles/:id', removeBlanks, (req, res, next) => {
 
     Motorcycle.findById(req.params.id)
         .then(handle404)
         .then((motorcycle) => {
-            // pass req and onject to requireownership
-            // it will throw an error if the current user isn't the owner
-            requireOwnership(req, motorcycle)
 
             // pass the result of Mongoose's `.update` to the next `.then`
             return motorcycle.updateOne(req.body.motorcycle)
@@ -93,12 +87,12 @@ router.patch('/motorcycles/:id', requireToken, removeBlanks, (req, res, next) =>
 
 //DESTROY----------------------
 //DELETE /motorcycles
-router.delete('/motorcycles/:id', requireToken, (req, res, next) => {
+router.delete('/motorcycles/:id', (req, res, next) => {
     Motorcycle.findById(req.params.id)
         .then(handle404)
         .then((motorcycle) => {
 
-            requireOwnership(req, motorcycle)
+
             // delete if the above didn't throw
             motorcycle.deleteOne()
         })
